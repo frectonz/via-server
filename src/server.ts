bd5376df8e7d4d6df6fba20179d938e391a7dd50
fraol0912 <fraol0912@gmail.com>
2021-11-29 07:57:01 +0300

    don't log env

--- src/server.ts
+++ src/server.ts
@@ -50,8 +50,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env);
-
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
