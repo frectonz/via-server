d7ef41d3e5ec7da80161c4390d9322f0ec479be8
fraol0912 <fraol0912@gmail.com>
2021-11-29 07:27:59 +0300

    fix from stackoverflow

--- src/server.ts
+++ src/server.ts
@@ -52,6 +52,8 @@ const main = async () => {
 
   const PORT = process.env.PORT || 5000;
 
+  console.log(process.env);
+
   app.listen(PORT, () =>
     console.log(
       `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
